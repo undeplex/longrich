119b58f39701e5ee8bf1e82b6fa4193f7fb38e8b
undeplex <undeplex@gmail.com>
2024-10-28 21:12:35 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,17 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Layout from '@/components/Layout';
 // _app.js or _app.tsx
-import { Open_Sans } from '@next/font/google';
 
-const openSans = Open_Sans({
-    subsets: ['latin'],
-    weight: ['400', '600', '700'],
-});
 
 
 function MyApp({ Component, pageProps }) {
     return (
         <div className="relative overflow-hidden">
-<main className={openSans.classNam}>
+
         <Layout>
                 <Component {...pageProps} />
         </Layout>
- </main>     
+    
         </div>
        
     );
